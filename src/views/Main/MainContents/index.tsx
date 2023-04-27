1a2601e0f005aa09d1fb83c2631e8d3dd23e0c35
kkh27373 <kkh27373@naver.com>
2023-04-27 17:52:15 +0900

    04.27.5:52

--- src/views/Main/MainContents/index.tsx
+++ src/views/Main/MainContents/index.tsx
@@ -2,9 +2,9 @@ import { Box, Typography } from "@mui/material";
 
 export default function MainContents(){
   return(
-    <Box sx={{ pt:'20px 120px', backgroundColor: 'rgba(0,0,0,0.05)' }}>
+    <Box sx={{ pt:'40px 120px', backgroundColor: 'rgba(0,0,0,0.05)' }} >
         <Box>
-            <Typography sx={{ fontSize: '24px', fontWeight: 500}}></Typography>
+            <Typography sx={{ fontSize:'20px', fontWeight:'400'  }}>회사 목록</Typography>
         </Box>
     </Box>
   );
