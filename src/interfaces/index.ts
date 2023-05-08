789e5f9baa1868ab071118650daa0f4005861646
kkh27373 <kkh27373@naver.com>
2023-05-08 17:51:10 +0900

    05.08최종

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,5 +1,19 @@
 import User from "./User.interface";
 import Company from "./Company.interface";
+
+export interface IPreviewItem {
+    img: string | null;
+    writerProfile: string;
+    writerNickname: string;
+    writerDate: string;
+    boardTitle: string;
+    boardContent: string;
+    likeCount: number;
+    commentCount: number;
+    viewCount: number;
+
+    boardNumber: number;
+}
 export interface IUser {
     email: string;
     password: string;
--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,1 +1,54 @@
-export const tmp = "";
+import {ICompany} from "src/interfaces/index"   
+
+export const COMPANYLISTTOP6: ICompany[] = [
+    {
+        email:'dsa@naver.com',
+        name:'작성자1',
+        telNumber: '010-1234-5678',
+        address: '부산광역시',
+        addressDetail: '부산 동구 부산진역',
+        profile:''
+    } ,{
+        email:'dsa@naver.com',
+        name:'작성자1',
+        telNumber: '010-1234-5678',
+        address: '부산광역시',
+        addressDetail: '부산 동구 부산진역',
+        profile:''
+    } ,{
+        email:'dsa@naver.com',
+        name:'작성자1',
+        telNumber: '010-1234-5678',
+        address: '부산광역시',
+        addressDetail: '부산 동구 부산진역',
+        profile:''
+    } ,{
+        email:'dsa@naver.com',
+        name:'작성자1',
+        telNumber: '010-1234-5678',
+        address: '부산광역시',
+        addressDetail: '부산 동구 부산진역',
+        profile:''
+    } ,{
+        email:'dsa@naver.com',
+        name:'작성자1',
+        telNumber: '010-1234-5678',
+        address: '부산광역시',
+        addressDetail: '부산 동구 부산진역',
+        profile:''
+    } ,{
+        email:'dsa@naver.com',
+        name:'작성자1',
+        telNumber: '010-1234-5678',
+        address: '부산광역시',
+        addressDetail: '부산 동구 부산진역',
+        profile:''
+    } ,{
+        email:'dsa@naver.com',
+        name:'작성자1',
+        telNumber: '010-1234-5678',
+        address: '부산광역시',
+        addressDetail: '부산 동구 부산진역',
+        profile:''
+    } 
+]
