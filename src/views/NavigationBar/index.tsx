c3a2914839329ebe7927232e23a75ca6162b38b3
Kim <rlagusxo96652@naver.com>
2023-05-13 16:40:43 +0900

    회사 회원 로그인 로그아웃  눌렀을 때 로그아웃 후 로그인화면으로 그리고 NavigationBar변화 까지

--- src/views/NavigationBar/index.tsx
+++ src/views/NavigationBar/index.tsx
@@ -1,15 +1,47 @@
 import { AppBar, Box, Button, FormControl, IconButton, InputAdornment, OutlinedInput, Toolbar, Typography } from "@mui/material";
 import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
+import { Company, User } from "src/interfaces";
 import { useCompanyStore } from "src/stores/companystores";
 import { useUserStore } from "src/stores/userstores";
 
-export default function NavigationBar_No_Login () {
-    const navigator = useNavigate();
-    
-  const { user } = useUserStore();
-  const {company} = useCompanyStore();
+
+export default function NavigationBar() {
+
+
+
+//  Hook         //
+
+  const navigator = useNavigate();
+  const { user,setUser } = useUserStore();
+  const {company,setCompany} = useCompanyStore();
   const [cookies, setCookies] = useCookies();
+//   쿠키 삭제가 쉽지 않네
+
+
+
+//    Event Handler  //
+
+
+const UserLogOutHandler = () =>{
+
+    setUser({} as User);   
+
+    console.log("User는 : "+user?.userEmail)
+    
+    navigator('/auth/login');
+
+}
+
+const CompanyLogOutHandler = () =>{
+
+    setCompany({} as Company);
+
+    navigator('/auth/login');
+}
+
+
+
 
     return (
         <Box sx={{flexGrow:1}}>
@@ -25,29 +57,36 @@ export default function NavigationBar_No_Login () {
                         구직 사이트
                     </Typography>
                     <Box sx={{ display: 'flex' }}>
-                                                {user && cookies.accessToken ? (
+                                                {user?.userAddress && cookies.accessToken ? (
                             <>
                                 <FormControl variant='outlined' sx={{ mr: '10px' }}>
-                                <Button variant="contained" color="secondary" onClick={() => navigator('/auth/login')}>
+                                <Button variant="contained" color="secondary" onClick = {UserLogOutHandler}>
                                     로그아웃
                                 </Button>
                                 </FormControl>
                                 <FormControl variant='outlined' sx={{ mr: '10px' }}>
-                                <Button variant="outlined" color="secondary" onClick={() => navigator('/auth/signup')}>
+                                <Button variant="outlined" color="secondary" onClick={() => navigator('/myPage')}>
                                     마이페이지
                                 </Button>
                                 </FormControl>
-                                {company && cookies.accessToken ? (
+                                
+                            </>
+                            ) : (company && cookies.accessToken ? (
                                 // "company"가 로그인한 경우 추가된 내용
+                            <>    
                                 <FormControl variant='outlined' sx={{ mr: '10px' }}>
-                                    <Button variant="outlined" color="secondary" onClick={() => navigator('/company/dashboard')}>
-                                    대시보드
+                                    <Button variant="outlined" color="secondary" onClick={CompanyLogOutHandler}>
+                                    로그아웃
                                     </Button>
                                 </FormControl>
-                                ) : null}
-                            </>
-                            ) : (
-                            <>
+                                <FormControl variant='outlined' sx={{ mr: '10px' }}>
+                                <Button variant="outlined" color="secondary" onClick={() => navigator('/myCompanyPage')}>
+                                    회사페이지
+                                </Button>
+                                </FormControl>
+                            </>    
+                                ) : (
+                            <>    
                                 <FormControl variant='outlined' sx={{ mr: '10px' }}>
                                 <Button variant="contained" color="secondary" onClick={() => navigator('/auth/login')}>
                                     로그인
@@ -58,8 +97,8 @@ export default function NavigationBar_No_Login () {
                                     회원가입
                                 </Button>
                                 </FormControl>
-                            </>
-                            )}
+                            </>    
+                        ))}
                     </Box >
                 </Toolbar>
             </AppBar>
