5668627a88c934a7f19712b6af1c7f1db0e44b77
Kim <rlagusxo96652@naver.com>
2023-05-24 17:50:29 +0900

    D-2

--- src/components/companyListCard/index.tsx
+++ src/components/companyListCard/index.tsx
@@ -1,9 +1,62 @@
-import { Avatar, Box, Card, CardActionArea, Typography } from "@mui/material";
+import { Avatar, Box, Card, CardActionArea, Typography,Button, FormControl } from "@mui/material";
+import axios, { AxiosResponse } from "axios";
+import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
 import { GetCompanyListResponseDto, GetListResponseDto } from "src/apis/response/company";
+import { authorizationHeader } from "src/contants/api";
+
+
+
+//  Hook //
+
 
 interface Props {
   companyListItem:  GetCompanyListResponseDto | GetListResponseDto;
+}
+
+const [cookies] = useCookies();
+const accessToken = cookies.accessToken;
+
+
+// Event Handler //
+const PopUpPage = ()=>{
+  
+
+  const width = 600; // 팝업 창 가로 크기
+    const height = 400; // 팝업 창 세로 크기
+    const left = window.innerWidth / 2 - width / 2; // 화면 중앙에 위치하기 위한 좌표 계산
+    const top = window.innerHeight / 2 - height / 2; // 화면 중앙에 위치하기 위한 좌표 계산
+    // ? 팝업창 화면 크기
+    const features = `width=${width},height=${height},left=${left},top=${top}`;
+
+    window.open("http://www.naver.com","_blank",features);
+
+    percentile();
+
+}
+
+const percentile = () =>{
+
+  const send_Data = {}
+  axios.post(,send_Data,authorizationHeader(accessToken))
+       .then((response)=>percentileResponseHandler(response))
+       .catch((error)=>percentileErrorHandler(error))
+
+}
+
+// Response Handler //
+const percentileResponseHandler = (response:AxiosResponse<any,any>) =>{
+
+}
+
+
+
+
+
+//  Error Handler //
+
+const percentileErrorHandler = (error:any) =>{
+
 }
 
 export default function CompanyListCard ({companyListItem}:Props) {
@@ -66,35 +119,19 @@ export default function CompanyListCard ({companyListItem}:Props) {
               {companyListItem.companyName}
             </Typography>
           </Box>
-          <Box sx={{ mt: "16px", mb: "16px",display:"flex",gap:"8px",justifyContent: "space-between" }}>
+          <Box sx={{ mt: "16px", mb: "16px",display:"flex",gap:"8px",justifyContent: "center" }}>
             
-              <Typography
-                sx={{
-                  mt: "5px",
-                  fontSize: "12px",
-                  fontWeight: 400,
-                  color: "rgba(0, 0, 0, 0.7)",
-                  
-                  // ?Typograph는 기본적으로 세로 정렬 ==> 가로 정렬을 원하면 inline요소를 추가 
-                  
-                }}
-              >
-                {companyListItem.applicantTotalScore}
-              </Typography>
-            
-            
-              <Typography
-                sx={{
-                  mt: "5px",
-                  fontSize: "12px",
-                  fontWeight: 400,
-                  color: "rgba(0, 0, 0, 0.7)",
-                  
-                  
-                }}
-              >
-                {companyListItem.applicantTotalScore}
-              </Typography>
+              <FormControl variant="outlined" sx={{ mr: '10px' }}>
+                <Button variant="contained" color="secondary" onClick={PopUpPage}>
+                  {companyListItem.applicantTotalScore}
+                </Button>
+              </FormControl>
+              <FormControl variant="outlined" sx={{ mr: '10px' }}>
+                <Button variant="contained" color="secondary" onClick={PopUpPage}>
+                  Percentile
+                </Button>
+              </FormControl>
+              
             
           </Box>
 
